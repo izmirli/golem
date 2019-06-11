d6c12e3c1355d6f474df04b6aa832d790c5262f2
Oren Izmirli <dev@izmirli.org>
2019-06-11 16:15:39 +0300

    get sender's info with User Profile API

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ from flask import Flask, request, send_from_directory
 
 app = Flask(__name__)
 app.debug = True
+my_debug = 4
 
 config: ConfigParser = ConfigParser()
 config.read(app.root_path + '/golem.ini')
-app.logger.debug('configuration from golem.ini:\n%s', '\n'.join(
-    sorted(
-        [f'{s}::{k}: {v}' for s in config.sections() for k, v in config.items(s)]
-    )
-))
+if 5 <= my_debug:
+    app.logger.debug('configuration from golem.ini:\n%s', '\n'.join(
+        sorted(
+            [f'{s}::{k}: {v}' for s in config.sections() for k, v in config.items(s)]
+        )
+    ))
 
 
 @app.route('/', methods=['GET'])
@@ -21,10 +23,12 @@ def handle_verification():
     """Verifies facebook webhook subscription
     Successful when verify_token is same as token sent by facebook app
     """
-    app.logger.debug('request.args: %s', request.args)
+    if 3 <= my_debug:
+        app.logger.debug('request.args: %s', request.args)
     if request.args.get('hub.verify_token', '') == config['TOKENS']['VERIFY_TOKEN']:
         # print("successfully verified")
-        app.logger.debug(f'successfully verified. challenge: %s', request.args.get('hub.challenge', ''))
+        if 1 <= my_debug:
+            app.logger.debug('successfully verified. challenge: %s', request.args.get('hub.challenge', ''))
         return request.args.get('hub.challenge', '')
     else:
         print("Wrong verification token!")
@@ -48,6 +52,7 @@ def handle_message():
                         # msg_timestamp = messaging_event["timestamp"]
                         # msg_id = messaging_event["message"]["mid"]
                         # msg_seq = messaging_event["message"]["seq"]
+                        get_sender_info(sender_id)
                         send_message_response(sender_id, parse_user_message(message_text))
     except KeyError as e:
         print(f'KeyError exception: {e}')
@@ -80,8 +85,33 @@ def send_message(sender_id, message_text):
                               ],
                           }
                       }))
+    if not r:
+        app.logger.warning('send_message failed - [%d] %s', r.status_code, r.text)
+    else:
+        if 3 <= my_debug:
+            app.logger.debug('send_message response - [%d] %s', r.status_code, r.text)
     # consider adding to data "messaging_type": "<MESSAGING_TYPE>",
     # MESSAGING_TYPE options: RESPONSE (default), UPDATE (proactive send), MESSAGE_TAG.
+
+
+def get_sender_info(psid):
+    """Get sender's info according to his PSID.
+
+    :param psid: PSID (sender's Page-scoped ID).
+    :return:
+    """
+    response = requests.post(
+        f'https://graph.facebook.com/{psid}',
+        params={"access_token": config['TOKENS']['PAGE_ACCESS_TOKEN'],
+                "fields": "name,first_name,last_name,profile_pic,gender"},
+        headers={"Content-Type": "application/json"},
+    )
+    if not response:
+        app.logger.warning('get_sender_info failed - [%d] %s', response.status_code, response.text)
+    else:
+        sender_info = response.json()
+        if 3 <= my_debug:
+            app.logger.debug('send_message response - [%d] %s', response.status_code, str(sender_info))
 
 
 def parse_user_message(user_text):
